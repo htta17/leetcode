aa5b7125b651411f35b86a860453ce21cc88af14
Moses Vo <factal.data@gmail.com>
2022-11-12 09:00:53 -0600

    Create minimum-size-subarray-sum.py

---
+++ Coding/SlidingWindow/Python/minimum-size-subarray-sum.py
@@ -0,0 +1,12 @@
+def min_sub_array_len(target, nums):
+    l = 0 
+    res = float('inf')
+    total = 0
+
+    for r in range(len(nums)):
+        total += nums[r]
+        while total >= target:
+            res = min(r - l + 1, res)
+            total -= nums[l]
+            l += 1
+    return 0 if res == float('inf') else res
